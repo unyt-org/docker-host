da700d85fa3f70ed77698401bbfb507f42bd9207
benStre <strehle.benedikt@gmail.com>
2022-08-21 12:54:41 +0200

    persistant container map, container status, start+stop

--- main.js
+++ main.js
@@ -11,30 +11,74 @@ import { exec } from "child_process";
 import fs from "fs";
 import { EndpointConfig } from "./endpoint-config.js";
 import DatexCloud from "./unyt_core/datex_cloud.js";
-import { Datex } from "./unyt_core/datex_runtime.js";
+import { Datex, eternal } from "./unyt_core/datex_runtime.js";
 import { constructor, expose, meta, property, replicator, root_extension, scope, sync } from "./unyt_core/legacy_decorators.js";
 import Logger from "./unyt_core/logger.js";
 const logger = new Logger("container manager");
 await DatexCloud.connect();
+var ContainerStatus;
+(function (ContainerStatus) {
+    ContainerStatus[ContainerStatus["STOPPED"] = 0] = "STOPPED";
+    ContainerStatus[ContainerStatus["STARTING"] = 1] = "STARTING";
+    ContainerStatus[ContainerStatus["RUNNING"] = 2] = "RUNNING";
+    ContainerStatus[ContainerStatus["STOPPING"] = 3] = "STOPPING";
+    ContainerStatus[ContainerStatus["FAILED"] = 4] = "FAILED";
+})(ContainerStatus || (ContainerStatus = {}));
 let Container = class Container {
     logger;
-    id;
+    initialized = false;
+    image;
+    id = '0';
     owner;
-    config;
-    constructor(id, owner, config) { }
-    construct(id, owner, config) {
-        this.id = id;
+    status = 0;
+    get container_name() {
+        return `${this.image}-inst-${this.id}`;
+    }
+    constructor(image, owner) { }
+    construct(image, owner) {
+        this.image = image;
         this.owner = owner;
-        this.config = config;
         this.logger = new Logger(this);
     }
     replicate() {
         this.logger = new Logger(this);
+        this.initialized = true;
+        this.updateAfterReplicate();
     }
     async start() {
-        this.logger.info("Starting Container");
+        this.logger.info("Starting Container " + this.container_name);
+        this.status = ContainerStatus.STARTING;
+        const running = await this.handleStart();
+        if (running)
+            this.status = ContainerStatus.RUNNING;
+        else
+            this.status = ContainerStatus.FAILED;
+        return running;
+    }
+    async init() {
         try {
-            await execCommand(`docker run --rm -d ${this.id}`);
+            await execCommand(`docker run -d --name ${this.container_name} ${this.image}`);
+        }
+        catch (e) {
+            this.logger.error("error while creating container");
+            return false;
+        }
+        this.initialized = true;
+        return true;
+    }
+    async updateAfterReplicate() {
+        if (this.status == ContainerStatus.STARTING)
+            this.start();
+        else if (this.status == ContainerStatus.STOPPING)
+            this.stop();
+    }
+    async handleStart() {
+        if (!this.initialized) {
+            if (!await this.init())
+                return false;
+        }
+        try {
+            await execCommand(`docker container start ${this.container_name}`);
         }
         catch (e) {
             this.logger.error("error while starting container");
@@ -50,11 +94,39 @@ let Container = class Container {
         }
         return true;
     }
+    async stop(force = true) {
+        this.logger.info("Stopping Container " + this.container_name);
+        this.status = ContainerStatus.STOPPING;
+        try {
+            await execCommand(`docker container ${force ? 'kill' : 'stop'} ${this.container_name}`);
+        }
+        catch (e) {
+            this.logger.error("error while stopping container", e);
+            this.status = ContainerStatus.FAILED;
+            return false;
+        }
+        this.status = ContainerStatus.STOPPED;
+        return true;
+    }
+    async remove() {
+        try {
+            await execCommand(`docker rm -f ${this.container_name}`);
+        }
+        catch (e) {
+            this.logger.error("error while removing container", e);
+            return false;
+        }
+        return true;
+    }
     async isRunning() {
-        const ps = await execCommand(`docker ps | grep ${this.id}`);
+        const ps = await execCommand(`docker ps | grep ${this.container_name}`);
         return !!ps;
     }
 };
+__decorate([
+    property,
+    __metadata("design:type", String)
+], Container.prototype, "image", void 0);
 __decorate([
     property,
     __metadata("design:type", String)
@@ -65,12 +137,12 @@ __decorate([
 ], Container.prototype, "owner", void 0);
 __decorate([
     property,
-    __metadata("design:type", EndpointConfig)
-], Container.prototype, "config", void 0);
+    __metadata("design:type", Number)
+], Container.prototype, "status", void 0);
 __decorate([
     constructor,
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint, EndpointConfig]),
+    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint]),
     __metadata("design:returntype", void 0)
 ], Container.prototype, "construct", null);
 __decorate([
@@ -85,20 +157,37 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", Promise)
 ], Container.prototype, "start", null);
+__decorate([
+    property,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], Container.prototype, "stop", null);
 Container = __decorate([
     sync,
-    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint, EndpointConfig])
+    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint])
 ], Container);
 let WorkbenchContainer = class WorkbenchContainer extends Container {
-    async start() {
-        if (!await super.start())
+    config;
+    get container_name() {
+        return `${this.image}`;
+    }
+    constructor(id, owner, config) { super(id, owner); }
+    constructWorkbenchContanier(id, owner, config) {
+        this.construct(id, owner);
+        this.config = config;
+        this.logger = new Logger(this);
+    }
+    async handleStart() {
+        if (!await super.handleStart())
             return false;
-        await new Promise(resolve => { setTimeout(() => resolve(), 8000); });
+        await new Promise(resolve => { setTimeout(() => resolve(), 6000); });
         try {
             await DatexCloud.pingEndpoint(this.config.endpoint);
         }
         catch (e) {
             this.logger.error("Workbench Endpoint not reachable");
+            await this.stop();
             return false;
         }
         this.logger.success("Workbench Endpoint is reachable");
@@ -107,34 +196,55 @@ let WorkbenchContainer = class WorkbenchContainer extends Container {
 };
 __decorate([
     property,
+    __metadata("design:type", EndpointConfig)
+], WorkbenchContainer.prototype, "config", void 0);
+__decorate([
+    constructor,
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", Promise)
-], WorkbenchContainer.prototype, "start", null);
+    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint, EndpointConfig]),
+    __metadata("design:returntype", void 0)
+], WorkbenchContainer.prototype, "constructWorkbenchContanier", null);
 WorkbenchContainer = __decorate([
-    sync
+    sync,
+    __metadata("design:paramtypes", [String, Datex.Addresses.Endpoint, EndpointConfig])
 ], WorkbenchContainer);
 let ContainerManager = class ContainerManager {
+    static async getContainers(meta) {
+        return containers.getAuto(meta.sender);
+    }
     static async createWorkbenchContainer(meta) {
         logger.info("Creating new Workbench Container for " + meta.sender);
         const config = new EndpointConfig();
         config.endpoint = Datex.Addresses.Endpoint.getNewEndpoint();
         const config_exported = Datex.Runtime.valueToDatexString(config, true, true, true);
-        const container_id = config.endpoint.toString().replaceAll('@', '').toLowerCase();
+        const image_name = 'unyt-workbench-' + config.endpoint.toString().replaceAll('@', '').toLowerCase();
         const username = meta.sender.toString().replaceAll('@', '');
-        logger.info("id: " + container_id);
+        logger.info("id: " + image_name);
         logger.info("config: " + config_exported);
+        logger.info("username: " + username);
         const tmp_dir = `res/config-files-${new Date().getTime()}`;
         await execCommand(`cp -r ./res/config-files ${tmp_dir}`);
         const writer = fs.createWriteStream(`${tmp_dir}/endpoint.dx`);
         writer.write(config_exported);
         writer.close();
-        await execCommand(`docker build --build-arg username=${username} --build-arg configpath=${tmp_dir} -f ./res/Dockerfile -t ${container_id} .`);
+        await execCommand(`docker build --build-arg username=${username} --build-arg configpath=${tmp_dir} -f ./res/Dockerfile -t ${image_name} .`);
         await execCommand(`rm -r ${tmp_dir}`);
-        const container = new WorkbenchContainer(container_id, meta.sender, config);
+        const container = new WorkbenchContainer(image_name, meta.sender, config);
+        container.start();
+        this.addContainer(meta.sender, container);
         return container;
     }
+    static addContainer(endpoint, container) {
+        containers.getAuto(endpoint).add(container);
+    }
 };
+__decorate([
+    meta(0),
+    expose,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], ContainerManager, "getContainers", null);
 __decorate([
     meta(0),
     expose,
@@ -146,6 +256,8 @@ ContainerManager = __decorate([
     root_extension,
     scope
 ], ContainerManager);
+const containers = (await eternal(Datex.Type.std.Map)).setAutoDefault(Set);
+logger.info("containers", containers);
 function execCommand(command) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import fs from "fs";
 import { EndpointConfig } from "./endpoint-config.js";
 import DatexCloud from "./unyt_core/datex_cloud.js";
-import { datex, Datex } from "./unyt_core/datex_runtime.js";
+import { datex, Datex, eternal } from "./unyt_core/datex_runtime.js";
 import {constructor, expose, meta, property, replicator, root_extension, scope, sync} from "./unyt_core/legacy_decorators.js";
 import Logger from "./unyt_core/logger.js";
 const logger = new Logger("container manager");
@@ -10,30 +10,84 @@ const logger = new Logger("container manager");
 await DatexCloud.connect();
 
 
+enum ContainerStatus {
+	STOPPED = 0,
+	STARTING = 1,
+	RUNNING = 2,
+	STOPPING = 3,
+	FAILED = 4
+}
 
+// parent class for all types of containers
 @sync class Container {
 
 	protected logger:Logger
+	protected initialized = false;
 
-	@property id: string
+	// docker container image + id
+	@property image: string
+	@property id: string = '0';
+
 	@property owner: Datex.Addresses.Endpoint
-	@property config: EndpointConfig
+	@property status: ContainerStatus = 0
 
-	constructor(id:string, owner: Datex.Addresses.Endpoint, config: EndpointConfig) {}
-	@constructor construct(id:string, owner: Datex.Addresses.Endpoint, config: EndpointConfig) {
-		this.id = id;
+	protected get container_name(){
+		return `${this.image}-inst-${this.id}`;
+	}
+
+	constructor(image:string, owner: Datex.Addresses.Endpoint) {}
+	@constructor construct(image:string, owner: Datex.Addresses.Endpoint) {
+		this.image = image;
 		this.owner = owner;
-		this.config = config;
 		this.logger = new Logger(this);
 	}
 	@replicator replicate(){
 		this.logger = new Logger(this);
+		this.initialized = true;
+		this.updateAfterReplicate();
 	}
 
 	@property async start(){
-		this.logger.info("Starting Container");
+		this.logger.info("Starting Container " + this.container_name);
+
+		// STARTING ...
+		this.status = ContainerStatus.STARTING;
+
+		// start => RUNNING or FAILED
+		const running = await this.handleStart();
+		if (running) this.status = ContainerStatus.RUNNING;
+		else this.status = ContainerStatus.FAILED;
+
+		return running;
+	}
+
+	// create docker for the first time
+	public async init(){
 		try {
-			await execCommand(`docker run --rm -d ${this.id}`)
+			await execCommand(`docker run -d --name ${this.container_name} ${this.image}`)
+		} catch (e) {
+			this.logger.error("error while creating container")
+			return false;
+		}
+		this.initialized = true;
+		return true;
+	}
+
+	protected async updateAfterReplicate(){
+		// continue start/stop if in inbetween state
+		if (this.status == ContainerStatus.STARTING) this.start();
+		else if (this.status == ContainerStatus.STOPPING) this.stop();
+	}
+
+	protected async handleStart(){
+		// first init docker container (only once)
+		if (!this.initialized) {
+			if (!await this.init()) return false;
+		}
+
+		// start the container
+		try {
+			await execCommand(`docker container start ${this.container_name}`)
 		} catch (e) {
 			this.logger.error("error while starting container")
 			return false;
@@ -48,28 +102,70 @@ await DatexCloud.connect();
 			this.logger.error("Container is not running")
 			return false;
 		}
+
+		return true;
+	}
+
+	@property async stop(force = true){
+		this.logger.info("Stopping Container " + this.container_name);
+
+		this.status = ContainerStatus.STOPPING;
+		try {
+			await execCommand(`docker container ${force?'kill':'stop'} ${this.container_name}`)
+		} catch (e) {
+			this.logger.error("error while stopping container",e);
+			// TODO FAILED or RUNNING?
+			this.status = ContainerStatus.FAILED;
+			return false;
+		}
+		this.status = ContainerStatus.STOPPED;
+		return true;
+	}
+
+	protected async remove(){
+		try {
+			await execCommand(`docker rm -f ${this.container_name}`)
+		} catch (e) {
+			this.logger.error("error while removing container",e);
+			return false;
+		}
 		return true;
 	}
 
 	private async isRunning(){
-		const ps = await execCommand(`docker ps | grep ${this.id}`);
+		const ps = await execCommand(`docker ps | grep ${this.container_name}`);
 		return !!ps;
 	}
 }
 
 @sync class WorkbenchContainer extends Container {
-	@property override async start(){
 
-		if (!await super.start()) return false;
+	@property config: EndpointConfig
+
+	// only one instance for each workbench docker container image
+	protected override get container_name(){
+		return `${this.image}`;
+	}
+
+	constructor(id:string, owner: Datex.Addresses.Endpoint, config: EndpointConfig) {super(id,owner)}
+	@constructor constructWorkbenchContanier(id:string, owner: Datex.Addresses.Endpoint, config: EndpointConfig) {
+		this.construct(id, owner)
+		this.config = config;
+		this.logger = new Logger(this);
+	}
+
+	override async handleStart(){
+		if (!await super.handleStart()) return false;
 
 		// check if endpoint is reachable
 		// wait some time TODO wait until endpoint calls
-		await new Promise<void>(resolve=>{setTimeout(()=>resolve(),8000)});
+		await new Promise<void>(resolve=>{setTimeout(()=>resolve(),6000)});
 		try {
 			await DatexCloud.pingEndpoint(this.config.endpoint);
 		}
 		catch (e) {
 			this.logger.error("Workbench Endpoint not reachable")
+			await this.stop(); // stop container again for consistant container state
 			return false;
 		}
 		this.logger.success("Workbench Endpoint is reachable");
@@ -77,8 +173,12 @@ await DatexCloud.connect();
 	}
 }
 
-
 @root_extension @scope class ContainerManager {
+
+	@meta(0)
+	@expose static async getContainers(meta:Datex.datex_meta):Promise<Set<Container>>{
+		return containers.getAuto(meta.sender);
+	}
 
 	@meta(0)
 	@expose static async createWorkbenchContainer(meta:Datex.datex_meta):Promise<WorkbenchContainer>{
@@ -90,11 +190,12 @@ await DatexCloud.connect();
 
 		// docker parameters
 		const config_exported = Datex.Runtime.valueToDatexString(config, true, true, true);
-		const container_id = config.endpoint.toString().replaceAll('@','').toLowerCase();
+		const image_name = 'unyt-workbench-' + config.endpoint.toString().replaceAll('@','').toLowerCase();
 		const username = meta.sender.toString().replaceAll('@','');
 
-		logger.info("id: " + container_id);
+		logger.info("id: " + image_name);
 		logger.info("config: " + config_exported);
+		logger.info("username: " + username);
 
 		// create new config directory to copy to docker
 		const tmp_dir = `res/config-files-${new Date().getTime()}`
@@ -104,21 +205,29 @@ await DatexCloud.connect();
 		writer.close();
 
 		// create docker container
-		await execCommand(`docker build --build-arg username=${username} --build-arg configpath=${tmp_dir} -f ./res/Dockerfile -t ${container_id} .`)
+		await execCommand(`docker build --build-arg username=${username} --build-arg configpath=${tmp_dir} -f ./res/Dockerfile -t ${image_name} .`)
 
 		// remove tmp directory
 		await execCommand(`rm -r ${tmp_dir}`);
 
 		// create WorkbenchContainer
-		const container = new WorkbenchContainer(container_id, meta.sender, config);
+		const container = new WorkbenchContainer(image_name, meta.sender, config);
+		container.start(); // start in background
+
+		// add container
+		this.addContainer(meta.sender, container);
 
 		return container;
 	}
 
-	
+	private static addContainer(endpoint:Datex.Addresses.Endpoint, container:Container) {
+		containers.getAuto(endpoint).add(container);
+	}
 
 }
 
+const containers: Map<Datex.Addresses.Endpoint, Set<Container>> = (await eternal(Datex.Type.std.Map)).setAutoDefault(Set);
+logger.info("containers", containers)
 
 
 function execCommand(command:string) {

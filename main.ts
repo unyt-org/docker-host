c40302590d0918b5b459761908ddb6711b7051c5
Jonas Strehle <jonas@strehles.info>
2024-01-30 00:41:25 +0100

    Add more logs

--- main.ts
+++ main.ts
@@ -725,8 +725,13 @@ enum ContainerStatus {
 					const appendOption = (option: string) => {
 						errorMessage += `${opt++}. ${option}\n`
 					}
-					if (!repoIsPublic) appendOption(`Make the repository publicly accessible (${this.gitOrigin === "GitHub" ? new URL(`./settings`, this.gitOriginURL) : new URL("./edit", this.gitOriginURL)}`);
-					appendOption(`Pass a ${this.gitOrigin} access token with --git-token=<token> (Generate at ${this.gitOrigin === "GitHub" ? `https://github.com/settings/personal-access-tokens/new` : new URL(`./-/settings/access_tokens`, this.gitOriginURL)} )`)
+					console.log(
+						this.gitOriginURL.toString(),
+						new URL("./edit", this.gitOriginURL).toString(),
+						new URL("./edit", this.gitOriginURL)
+					)
+					if (!repoIsPublic) appendOption(`Make the repository publicly accessible (${this.gitOrigin === "GitHub" ? new URL(`./settings`, this.gitOriginURL) : new URL("./edit", this.gitOriginURL)})`);
+					appendOption(`Pass a ${this.gitOrigin} access token with --git-token=<token> (Generate at ${this.gitOrigin === "GitHub" ? `https://github.com/settings/personal-access-tokens/new` : new URL(`./-/settings/access_tokens`, this.gitOriginURL)})`)
 					if (sshKey) appendOption(`Add the following SSH key to your repository (${this.gitOrigin === "GitHub" ? new URL(`./settings/keys/new`, this.gitOriginURL) : new URL(`./-/settings/repository`, this.gitOriginURL)}): \n\n${ESCAPE_SEQUENCES.GREY}${sshKey}${ESCAPE_SEQUENCES.RESET}\n`);
 					this.errorMessage = errorMessage;
 					throw e;
